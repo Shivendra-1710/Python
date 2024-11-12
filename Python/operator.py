7f391b077058359a4c571350203a90def024e9fd
Shivendra-1710 <abhi83514@gmail.com>
2024-11-12 06:40:30 +0000

    config

---
+++ Python/operator.py
@@ -0,0 +1,19 @@
+a = 45
+b = 23
+
+print("a > b:",a > b)
+print("a < b:",a < b)
+print("a >= b:",a >= b)
+print("a <= b:",a <= b)
+print("a = b:",a == b)
+print("a != b:",a != b)
+
+
+# Logical Operator : And, Or, Not operator
+
+x = True
+y = False
+
+print("x and y is", x and y)
+print("x or y is", x or y)
+print("not y is ", not y)
