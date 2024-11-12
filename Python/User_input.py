1f4d19e3bbfd5363c490b15b4d3cd3097311a969
Shivendra-1710 <abhi83514@gmail.com>
2024-11-12 06:51:38 +0000

    config

---
+++ Python/User_input.py
@@ -0,0 +1,10 @@
+# a = input("Enter prompt to display to user")
+
+# Make a calculator for multiplication of these two number by taking input from an user
+
+a = int(input("Enter the first number: "))
+b = float(input("Enter the second Number: "))
+
+c = a * b
+
+print(c)
