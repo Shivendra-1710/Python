a341bb7ff073c5c13a7caccd1c8c6ad0faa9508a
Shivendra-1710 <abhi83514@gmail.com>
2024-11-12 07:02:18 +0000

    config

--- Python/User_input.py
+++ Python/User_input.py
@@ -8,3 +8,19 @@ b = float(input("Enter the second Number: "))
 c = a * b
 
 print(c)
+
+# Check the class type 
+
+print(type(a))
+print(type(b))
+
+
+# WAP to take marks of a user In English, Science and Maths and print he average of these marks.
+
+English_marks = int(input("Enter the english's marks: "))    
+Maths_marks = int(input("Enter the maths's marks: "))
+Science_marks = int(input("Enter the science's marks: "))
+
+Average_marks = (English_marks + Maths_marks + Science_marks) / 3
+print(Average_marks)
+ 
