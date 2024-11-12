a0aa9325c79267013dd4193a03798702fa3eefac
Shivendra-1710 <abhi83514@gmail.com>
2024-11-12 06:19:58 +0000

    config

---
+++ Python/identifier.py
@@ -0,0 +1,17 @@
+# Valid Identifier
+
+age = 24
+name = "Abhi"
+last_name = "Rajput"
+_id = "24*345"
+name2 = "Shiv"
+
+
+# Invalid identifier
+
+2name = "Abhi"
+marks$ = 98
+class = "Hello"
+
+print age
+print name 
