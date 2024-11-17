6d294a0dd0032e4fa7d804602a46c97593353306
Shivendra-1710 <abhi83514@gmail.com>
2024-11-17 21:33:50 +0000

    config

---
+++ Python/python_range.py
@@ -0,0 +1,12 @@
+# range() function is used to generate a sequence of numbers
+# range(10) will generate number from 0 to 9(10 numbers) - [0,1,2,3,4,5,6,7,8,9]
+
+
+# Also define as range(start,stop,step size)   as step size is default to 1.
+
+# code
+
+a = list(range(2,10))        
+
+print(a)
+
